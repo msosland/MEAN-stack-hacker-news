1c3f906c7a4ad3c11451fec73396e3b9bd0e8a90
Marshall Sosland <marshall.sosland@gmail.com>
2016-03-26 16:54:15 -0500

    move html to inline script

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,16 @@
-angular.module('newsApp', []);
+angular.module('newsApp', ['ui.router'])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'home.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+  }]);
 
