7b7e2b633f8530c5279e4f25bbf5d73a5c5a0c23
Marshall Sosland <marshall.sosland@gmail.com>
2016-03-27 16:54:24 -0500

    finish persistance of comments

--- public/javascripts/scripts/services/posts.js
+++ public/javascripts/scripts/services/posts.js
@@ -4,17 +4,23 @@ angular.module('newsApp')
 .factory('posts', ['$http', function($http) {
   var o = {
     posts: [
-      {title: 'post 1', upvotes: 5, comments: []},
-      {title: 'post 2', upvotes: 2, comments: []},
-      {title: 'post 3', upvotes: 15, comments: []},
-      {title: 'post 4', upvotes: 9, comments: []},
-      {title: 'post 5', upvotes: 4, comments: []}
+      // {title: 'post 1', upvotes: 5, comments: []},
+      // {title: 'post 2', upvotes: 2, comments: []},
+      // {title: 'post 3', upvotes: 15, comments: []},
+      // {title: 'post 4', upvotes: 9, comments: []},
+      // {title: 'post 5', upvotes: 4, comments: []}
     ]
   };
 
   o.getAll = function() {
     return $http.get('/posts').success(function(data){
       angular.copy(data, o.posts);
+    });
+  };
+
+  o.get = function(id) {
+    return $http.get('/posts/' + id).then(function(res){
+      return res.data;
     });
   };
 
@@ -28,6 +34,17 @@ angular.module('newsApp')
     return $http.put('/posts/' + post._id + '/upvote').success(function(data){
       post.upvotes += 1;
     });
+  };
+
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment);
+  };
+
+  o.upvoteComment = function(post, comment) {
+    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
+      .success(function(data){
+        comment.upvotes += 1;
+      });
   };
 
   return o;
