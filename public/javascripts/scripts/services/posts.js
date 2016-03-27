95da3726bde2a3431954791174a11eb8c4619c2c
Marshall Sosland <marshall.sosland@gmail.com>
2016-03-27 16:32:29 -0500

    add upvoting for posts persistance

--- public/javascripts/scripts/services/posts.js
+++ public/javascripts/scripts/services/posts.js
@@ -11,10 +11,24 @@ angular.module('newsApp')
       {title: 'post 5', upvotes: 4, comments: []}
     ]
   };
+
   o.getAll = function() {
     return $http.get('/posts').success(function(data){
       angular.copy(data, o.posts);
     });
   };
+
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data){
+      o.posts.push(data);
+    });
+  };
+
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote').success(function(data){
+      post.upvotes += 1;
+    });
+  };
+
   return o;
 }]);
--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,18 @@ router.param('post', function(req, res, next, id) {
   });
 });
 
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function(err, comment) {
+    if(err){ return next(err); }
+    if(!comment){ return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
 router.get('/posts/:post', function(req, res, next) {
   req.post.populate('comments', function(err, post) {
     if(err) {return next(err); }
@@ -71,17 +83,6 @@ router.post('/posts/:post/comments', function(req, res, next) {
   });
 });
 
-router.param('comment', function(req, res, next, id) {
-  var query = Comment.findById(id);
-
-  query.exec(function(err, comment) {
-    if(err){ return next(err); }
-    if(!comment){ return next(new Error('can\'t find comment')); }
-
-    req.comment = comment;
-    return next();
-  });
-});
 
 router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   req.comment.upvote(function(err, comment) {
